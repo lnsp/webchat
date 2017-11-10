b6500bea5c24ac6373d79b64f5f7d2901bf91533
Lennart Espe <lnsp@users.noreply.github.com>
2017-11-10 13:46:09 +0100

    Add support for channels

--- main.go
+++ main.go
@@ -4,19 +4,51 @@ import (
 	"net/http"
 	"os"
 
+	logrus "github.com/Sirupsen/logrus"
 	"github.com/moby/moby/pkg/namesgenerator"
 	"golang.org/x/net/websocket"
 )
 
 type Channel struct {
-	Participants []User
+	Participants map[string]*User
 	Name         string
 }
 
-func (c Channel) Broadcast(msg Message) {
+func (c *Channel) Broadcast(msg Message) {
+	logrus.WithFields(logrus.Fields{
+		"channel": c.Name,
+		"sender":  msg.Sender,
+		"message": msg.Text,
+	}).Info("Broadcasting message to users")
 	for _, p := range c.Participants {
 		p.Send(msg)
 	}
+}
+
+func (c *Channel) Join(u *User) {
+	logrus.WithFields(logrus.Fields{
+		"channel": c.Name,
+		"user":    u.Name,
+	}).Info("User joined channel")
+	c.Participants[u.Name] = u
+	c.Broadcast(Message{
+		Sender:  "server",
+		Text:    u.Name + " joined the channel",
+		Channel: c.Name,
+	})
+}
+
+func (c *Channel) Leave(u *User) {
+	logrus.WithFields(logrus.Fields{
+		"channel": c.Name,
+		"user":    u.Name,
+	}).Info("User left channel")
+	delete(c.Participants, u.Name)
+	c.Broadcast(Message{
+		Sender:  "server",
+		Text:    u.Name + " left the channel",
+		Channel: c.Name,
+	})
 }
 
 type Message struct {
@@ -26,36 +58,39 @@ type Message struct {
 }
 
 type User struct {
-	Conn *websocket.Conn
-	Name string
+	Conn      *websocket.Conn
+	Name      string
+	SendingTo *Channel
 }
 
-func (p User) Join(channel *Channel) {
-	channel.Participants = append(channel.Participants, p)
-	channel.Broadcast(Message{
-		Sender: "server",
-		Text:   "User " + p.Name + " joined the server.",
-	})
+func (p *User) Watch() {
+	logrus.WithField("name", p.Name).Info("Watching user input")
 	var text string
 	for {
 		if err := websocket.Message.Receive(p.Conn, &text); err != nil {
 			break
 		}
+		logrus.WithFields(logrus.Fields{
+			"message": text,
+			"name":    p.Name,
+		}).Info("Received message from user")
 		msg := Message{
 			Sender:  p.Name,
 			Text:    text,
-			Channel: channel.Name,
+			Channel: p.SendingTo.Name,
 		}
-		channel.Broadcast(msg)
+		p.SendingTo.Broadcast(msg)
 	}
+	p.SendingTo.Leave(p)
+	logrus.WithField("name", p.Name).Info("Closing connection")
 }
 
-func (p User) Send(msg Message) error {
+func (p *User) Send(msg Message) error {
 	return websocket.JSON.Send(p.Conn, msg)
 }
 
-func NewUser(conn *websocket.Conn) User {
-	return User{
+func NewUser(conn *websocket.Conn) *User {
+	return &User{
 		Name: namesgenerator.GetRandomName(0),
 		Conn: conn,
 	}
@@ -64,16 +99,21 @@ func NewUser(conn *websocket.Conn) User {
 func NewChannel(name string) *Channel {
 	return &Channel{
 		Name:         name,
-		Participants: []User{},
+		Participants: map[string]*User{},
 	}
 }
 
 func main() {
-	defaultChan := NewChannel("default")
+	mainChannel := NewChannel("main")
 	http.Handle("/", http.FileServer(http.Dir("static")))
 	http.Handle("/chat/", websocket.Handler(func(conn *websocket.Conn) {
+		logrus.WithField("remote", conn.RemoteAddr()).Info("Connected with client")
 		user := NewUser(conn)
-		user.Join(defaultChan)
+		logrus.WithField("name", user.Name).Info("Generated new user")
+		defer user.Watch()
+
+		user.SendingTo = mainChannel
+		mainChannel.Join(user)
 	}))
 
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
